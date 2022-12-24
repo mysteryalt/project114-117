9771c603c2bd979e828c60dd91788469eede1682
mysteryalt <86229694+mysteryalt@users.noreply.github.com>
2022-12-24 14:48:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
 function setup() {
     canvas = createCanvas(400,300);
     canvas.center()
-    
+    video = createCapture(VIDEO);
+video.size(400,300);
+video.hide();
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses );
+}
+
+function draw() {
+image(video , 0 , 0 , 400 , 300);
+
+}
+
+function preload() {
+
+}
+
+function modelLoaded() {
+    console.log("PoseNet is intialised")
+}
+
+function gotPoses(results) {
+    if (results.length > 0 ) {
+    console.log(results);
+    console.log("nose x = " + results[0].pose.nose.x);
+    console.log("nose y = " + results[0].pose.nose.y)
+    }
     }
     
     
